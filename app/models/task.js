852ae1c291b9897e137f92b151344aed8f5f416a
Juliano Moura <julianodeveloper@gmail.com>
2018-04-08 15:17:42 -0300

    criando metodos de criação, edição, alteração e remoção de projetos relacionados com tasks

--- app/models/task.js
+++ app/models/task.js
@@ -1,14 +1,37 @@
-/**
- * Model Tasks
- */
+const mongoose = require('../database/app');
+const bcrypt = require('bcryptjs');
 
-var mongoose = require('../database/app');
-var Schema = mongoose.Schema
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true,
+    },
+    project: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Project',
+            require: true,
+        }
+    ],
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        require: true,
+    },    
+    completed: {
+        type: Boolean,
+        require: true,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
-var taskSchema = new Schema({
-   nome: String,
-   descricao: String,
-   status: Boolean
-})  
-
-module.exports = mongoose.model('Task', taskSchema)
+const Task = mongoose.model('Task', TaskSchema);
+module.exports = Task;
